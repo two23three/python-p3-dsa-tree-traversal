c25f3ffac72c247ab325e7ac68f8a7a0d40a3795
Tulley Mwenda <Tulleyteyez@gmail.com>
2024-06-07 12:16:33 +0300

    done

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,14 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    nodes_to_traverse= [self.root]
+
+    while nodes_to_traverse:
+      node = nodes_to_traverse.pop()
+      if node['id'] == id:
+        return node
+      if node['children']:
+       
+        nodes_to_traverse.extend(node['children'])
+
+    return None
